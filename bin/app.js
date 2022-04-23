4d012e5540c2d5f89607c93893e92226948d5cc5
BRAVO68WEB <jbandyopadhayaya@gmail.com>
2022-04-23 22:41:10 +0530

    Playing around

--- bin/app.js
+++ bin/app.js
@@ -1,1 +1,22 @@
-console.log("Hello!");
+#!/usr/bin/env node
+import chalk from "chalk";
+import boxen from "boxen";
+import yargs from "yargs";
+
+const options = yargs().usage("Usage: -n <name>").option("n", {
+  alias: "name",
+  describe: "Your name",
+  type: "string",
+  demandOption: true,
+}).argv;
+const greeting = `Hello, ${options.name}!`;
+
+const boxenOptions = {
+  padding: 1,
+  margin: 1,
+  borderStyle: "round",
+  borderColor: "green",
+  backgroundColor: "#555555",
+};
+
+console.log(greeting);
